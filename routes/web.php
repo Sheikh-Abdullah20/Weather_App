dbd35de1cc457e70fdbc887996e22e51c82da1de
Sheikh-Abdullah20 <162795515+Sheikh-Abdullah20@users.noreply.github.com>
2024-08-15 12:12:52 +0500

    Commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\WeatherController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::match(['get','post'],'weather',[WeatherController::class, 'index']);
